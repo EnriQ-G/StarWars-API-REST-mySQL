c54dfd0567456a22d046520a54d8f6d22b7726cc
Enrique Graniel <legraniel.91@gmail.com>
2023-03-28 13:03:30 -0600

    Creación de rutas, faltan pruebas de funcionalidad

---
+++ src/Routes/starwars.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { methods as starwarsController } from "./../controllers/starwars.controllers"
+
+const router = Router();
+
+router.get("/", starwarsController.getPeople);
+router.get("/:id", starwarsController.getPerson);
+router.post("/", starwarsController.addPerson)
+router.put("/:id", starwarsController.updatePerson);
+router.delete("/:id", starwarsController.deletePerson)
+
+
+export default router;
